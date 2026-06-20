f6db9442101e71c6467d2f3994bc3fadca4e42e2
Colin <colinleonghongseng@gmail.com>
2026-06-20 10:47:17 +0800

    comment out template code

--- fastapi-backend/app/dependencies.py
+++ fastapi-backend/app/dependencies.py
@@ -1,10 +1,10 @@
 from app.db.database import get_db
 from app.core.config import get_settings
-from app.core.security import ph, validate_api_key
+# from app.core.security import ph, validate_api_key
 
 __all__ = [
     'get_db',
     'get_settings',
-    'ph',
-    'validate_api_key'
+    # 'ph',
+    # 'validate_api_key'
 ]
--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.routers import api_key, auth, predict
+# from app.routers import api_key, auth, predict
 
 app = FastAPI()
 
-app.include_router(api_key.router)
-app.include_router(auth.router)
-app.include_router(predict.router)
+# app.include_router(api_key.router)
+# app.include_router(auth.router)
+# app.include_router(predict.router)
 
 @app.get('/health')
 async def health():
